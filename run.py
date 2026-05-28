3f0c69db7caf78570eecaf1d6393e21ac526e6d9
zhuobinggang <zhuobinggang@gmail.com>
2026-05-28 14:53:59 +0900

    准备真正训练

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from agent_navigator import *
 print(f'TEMP_SAVE_DIR: {TEMP_SAVE_DIR}')
-train_repeat(testing=True)
-test_all_checkpoints(testing=True)
+train_repeat(testing=False)
+test_all_checkpoints(testing=False)
